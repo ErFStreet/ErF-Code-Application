41f6cb7cd1d71061d0b0e19374923e01c082624d
Erfan-Edalati <erfannstreet@gmail.com>
2024-03-07 23:27:00 +0330

    Update

--- src/Client/Application/Application/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-﻿global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using Microsoft.JSInterop;
-global using Domain.ViewModels.Role;
-global using System.Net.Http.Json;
-global using Application.Instructure.Extension;
-global using SoftMax.Result.Instructure;
-global using Blazored.LocalStorage;
---
+++ src/Client/ClientApplication/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.JSInterop;
+global using Domain.ViewModels.Role;
+global using System.Net.Http.Json;
+global using ClientApplication.Instructure.Extension;
+global using SoftMax.Result.Instructure;
+global using Blazored.LocalStorage;
